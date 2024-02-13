a5d1f8453a9a6eb58fcc09ffc4248e53821560cd
Dominik <dominik2711@gmail.com>
2024-02-13 13:48:54 +0100

    Ugly spaghetti - needs complete rewrite. But it works.

--- src/brk_robot006/main.py
+++ src/brk_robot006/main.py
@@ -26,16 +26,18 @@ from dotenv import load_dotenv
 from loguru import logger
 from playwright.sync_api import sync_playwright
 
+load_dotenv()
+log_path = os.getenv("LOG_PATH")
+logger.add(log_path, format="{time} {level} {message}", level="DEBUG")
+start_time = time.time()
 server_name = os.environ["COMPUTERNAME"]
 user = getpass.getuser()
-load_dotenv()
 server_prefix = os.getenv("SERVER_PREFIX")
 folder_data = Path(os.getenv("FOLDER_DATA"))
 sapshcut_path = Path(os.getenv("SAPSHCUT_PATH"))
 pam_path = os.getenv("PAM_PATH")
 ri_url = os.getenv("RI_URL")
-log_path = os.getenv("LOG_PATH")
-logger.add(log_path, format="{time} {level} {message}", level="DEBUG")
+
 
 # Right now devmode has no real use.
 
@@ -54,7 +56,7 @@ if devmode == 0:
     logger.info("starting in production with report generation and download")
 
 if downloadmode == 0:
-    bestillingsnavn = user + "_" + "persistent_dev_data"
+    bestillingsnavn = user + "_" + "persistent_test_data"
 else:
     bestillingsnavn = user + "_" + pendulum.now().strftime("%Y%m%d%H%M%S")
 
@@ -193,16 +195,20 @@ def read_report(folder_data_session: Path, bestillingsnavn: str) -> pd.core.fram
             sep="\t",
             header=None,
             encoding="windows-1252",
-            skiprows=12,
+            skiprows=13,
             dtype=data_types,
             # usecols=[1, *range(3, 17)],
         )
         df.columns = df.iloc[0]
         df = df.drop(df.index[0])
-        df = df.dropna(axis=1, how="all")
+        # df = df.dropna(axis=1, how="all")
         df.columns = df.columns.str.lower().str.replace("[-.,1æøå ]", "", regex=True)
         df["samletpct"] = df["samletpct"].str.replace(",", ".").astype(float)
         logger.info(f"Successfully read ansforhold data from {report_path}")
+
+        nan_columns = [col for col in df.columns if pd.isna(col)]
+        df = df.drop(columns=nan_columns)
+
         return df
 
     except Exception as e:
@@ -213,14 +219,15 @@ def read_report(folder_data_session: Path, bestillingsnavn: str) -> pd.core.fram
 
 df = read_report(folder_data_session=folder_data_session, bestillingsnavn=bestillingsnavn)
 
+
 # ------------------------------- Rens rapport ------------------------------- #
 # Fjern whitespace fra objects-columns
 df_obj = df.select_dtypes(["object"])
 df[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
 
 # ----------------------------------- TEMP ----------------------------------- #
-if devmode == 1:
-    df = df[:10]
+# if devmode == 1:
+#     df = df[:10]
 # --------------------------------- END TEMP --------------------------------- #
 
 # --------- Get df row count that must persist throughout the session -------- #
@@ -254,9 +261,9 @@ def validate_dataframe(dataframe, col_count, row_count=None, dataframe_name="", 
         logger.error(f"Validation failed: {e}")
 
 
-validate_dataframe(
-    dataframe=df, col_count=15, row_count=persistent_df_row_count, dataframe_name="df", manummer_column="manummer"
-)
+# validate_dataframe(
+#    dataframe=df, col_count=15, row_count=persistent_df_row_count, dataframe_name="df", manummer_column="manummer"
+# )
 
 
 # ---------------------------------------------------------------------------- #
@@ -289,10 +296,6 @@ def har_medarbejder_pension_i_forvejen(df: pd.DataFrame) -> pd.DataFrame:
 
 
 df = har_medarbejder_pension_i_forvejen(df)
-
-validate_dataframe(
-    dataframe=df, col_count=17, row_count=persistent_df_row_count, dataframe_name="df", manummer_column="manummer"
-)
 
 
 # ---------------------------------------------------------------------------- #
@@ -335,10 +338,6 @@ def er_medarbejder_under_21(df: pd.DataFrame) -> pd.DataFrame:
 
 
 df = er_medarbejder_under_21(df)
-
-validate_dataframe(
-    dataframe=df, col_count=18, row_count=persistent_df_row_count, dataframe_name="df", manummer_column="manummer"
-)
 
 
 # ---------------------------------------------------------------------------- #
@@ -431,6 +430,10 @@ def read_single_ansforhold(manummer: str, folder_data_session: Path, bestillings
         df_ansforhold = df_ansforhold.drop(df_ansforhold.index[0])
         df_ansforhold.columns = df_ansforhold.columns.str.lower().str.replace("[-.,1æøå ]", "", regex=True)
         logger.info(f"Successfully read ansforhold data from {path_csv_ansforhold}")
+
+        nan_columns = [col for col in df_ansforhold.columns if pd.isna(col)]
+        df_ansforhold = df_ansforhold.drop(columns=nan_columns)
+
         return df_ansforhold
 
     except Exception as e:
@@ -476,8 +479,8 @@ def filter_df_ansforhold(df_ansforhold) -> pd.DataFrame:
             parts = [int(part) for part in lonklasse.split("-")]
 
             if parts[0] == 1:
-                return 9990 <= parts[1] <= 9999
-            if parts[0] == 7:
+                return 9990 <= parts[1] <= 9999  # noqa: PLR2004
+            if parts[0] == 7:  # noqa: PLR2004
                 if str(parts[1])[0] == "4":
                     return True
                 if str(parts[1])[0] == "9":
@@ -500,6 +503,7 @@ def sort_df_filtered(df_filtered) -> pd.DataFrame:
         df_filtered["startdato"] = pd.to_datetime(df_filtered["startdato"], format="%d.%m.%Y")
         df_sorted = df_filtered.sort_values(by=["startdato"])
 
+        # Select the oldest row
         if not df_sorted.empty:
             df_sorted = df_sorted.iloc[[0]]
         return df_sorted
@@ -509,14 +513,16 @@ def sort_df_filtered(df_filtered) -> pd.DataFrame:
         return pd.DataFrame()
 
 
-# -- Funktion der tjekker om der er mere end 12 gyldige måneder i ansforlob -- #
-def over_12_gyldige_ansfh01(df_filtered) -> bool:
+# Funktion der tilføjer kolonner til df_filtered. Gemmer df_filtered under df_over_12_gyldige_ansfh01.
+# tjekker om der er mere end 12 gyldige måneder i ansforlob -- #
+def over_12_gyldige_ansfh01(df_filtered) -> tuple[int, pd.DataFrame]:
     """I forhold til beregning af gyldige måneder for månedslønsansættelser,
     hvis startperioden for en måned ikke er den 1. skal måneden ikke tælles med,
     i dette tilfælde er det næstkommende måned der gælder.
     Hvis medarbejderen slutter midt i en måned, tælles denne måned med.
     """
-
+    if df_filtered.empty:
+        return 0, df_filtered
     df_over_12_gyldige_ansfh01 = df_filtered.copy()
 
     # Startdato skal være den 1. i måneden. Stopdata kan være hvilken som helst dag i måneden.
@@ -541,14 +547,16 @@ def over_12_gyldige_ansfh01(df_filtered) -> bool:
     )
 
     # Antal gyldige måneder pr række stopdato_year_month - startdato_year_month
-    df_over_12_gyldige_ansfh01["antal_maaneder"] = df_over_12_gyldige_ansfh01["stopdato_year_month"].astype(
-        "int64"
-    ) - df_over_12_gyldige_ansfh01["startdato_year_month"].astype("int64")
+    df_over_12_gyldige_ansfh01["antal_maaneder"] = (
+        df_over_12_gyldige_ansfh01["stopdato_year_month"].astype("int64")
+        - df_over_12_gyldige_ansfh01["startdato_year_month"].astype("int64")
+        + 1
+    )
 
     # Sum the number of months
     sum_antal_maaneder = df_over_12_gyldige_ansfh01["antal_maaneder"].sum()
 
-    return sum_antal_maaneder
+    return sum_antal_maaneder, df_over_12_gyldige_ansfh01
 
 
 # --- Kør de 3 funktioner (read, filter, sort) i et loop over alle manumre --- #
@@ -560,12 +568,11 @@ def process_all_ansforhold(df: pd.DataFrame, folder_data_session: Path, bestilli
     """
     Loops over manummer_list and runs all the single functions.
     """
-    # global all_rows
-    # all_rows = []  # List to store the single-row DataFrames, mostly for debugging purposes
     manummer_list = (df["manummer"]).tolist()
 
     # Initialize columns in df
     df["oprettet_pension_maaned"] = ""
+    df["maanedsloen_med_pension_fra_dato"] = ""
     df["antal_gyldige_maaneder_ansfh01"] = ""
     df["more_than_12_months_ansfh01"] = ""
 
@@ -574,24 +581,30 @@ def process_all_ansforhold(df: pd.DataFrame, folder_data_session: Path, bestilli
         # Read and process the downloaded data
         df_ansforhold = read_single_ansforhold(manummer, folder_data_session, bestillingsnavn)
 
-        # Filter and sort the DataFrame
-        df_filtered = filter_df_ansforhold(df_ansforhold)
+        # Filter the DataFrame, if it's not empty
+        if not df_ansforhold.empty:
+            df_filtered = filter_df_ansforhold(df_ansforhold)
 
-        df_sorted = sort_df_filtered(df_filtered)
+        # Sort the DataFrame
+        if not df_filtered.empty:
+            df_sorted = sort_df_filtered(df_filtered)
+            nan_columns = [col for col in df_sorted.columns if pd.isna(col)]
+            df_sorted = df_sorted.drop(columns=nan_columns)
+            # convert to string
+            maanedsloen_med_pension_fra_dato = df_sorted["startdato"].dt.strftime("%Y-%m").to_string(index=False)
+            bull = len(df_sorted) == 1
+        else:
+            maanedsloen_med_pension_fra_dato = ""
+            bull = False
 
-        # Append the sorted DataFrame to the list
-        if df_sorted.columns[0] is np.nan:
-            df_sorted = df_sorted.drop(df_sorted.columns[0], axis=1)
-
-        # all_rows.append(df_sorted)
-
-        bull = len(df_sorted) == 1
-
-        # Update df directly depending on if df_sorted has exactly one row
+        # Update df depending on if df_sorted has exactly one row
         df.loc[df["manummer"] == manummer, "oprettet_pension_maaned"] = bull
 
+        # Update df with the startdato from df_sorted
+        df.loc[df["manummer"] == manummer, "maanedsloen_med_pension_fra_dato"] = maanedsloen_med_pension_fra_dato
+
         # Check if there are more than 12 gyldige ansfh01
-        sum_antal_maaneder = over_12_gyldige_ansfh01(df_filtered)
+        sum_antal_maaneder = over_12_gyldige_ansfh01(df_filtered)[0]
         df.loc[df["manummer"] == manummer, "antal_gyldige_maaneder_ansfh01"] = sum_antal_maaneder
 
         # Check if there are more than 12 gyldige ansfh01
@@ -603,10 +616,6 @@ def process_all_ansforhold(df: pd.DataFrame, folder_data_session: Path, bestilli
     # logger.error(f"Error processing manummer {manummer}: {e}", exc_info=True)
     # Continue with the next iteration
     # continue
-
-    # Concatenate all single-row DataFrames into one DataFrame
-    # all_rows = pd.concat(all_rows, ignore_index=False)
-
     return df
 
 
@@ -625,10 +634,6 @@ df = process_all_ansforhold(
     df=df,
     folder_data_session=folder_data_session,
     bestillingsnavn=bestillingsnavn,
-)
-
-validate_dataframe(
-    dataframe=df, col_count=21, row_count=persistent_df_row_count, dataframe_name="df", manummer_column="manummer"
 )
 
 
@@ -826,8 +831,9 @@ def parse_ri_html_report_to_dataframe(mhtml_path) -> None:
     return df_mhtml
 
 
-load_dotenv(override=True)
-cpr = os.getenv("CPR")
+# load_dotenv(override=True)
+# cpr = os.getenv("CPR")
+# manummer = os.getenv("MANUMMER")
 
 
 def process_all_anshistorik(folder_data_session, df):
@@ -836,7 +842,8 @@ def process_all_anshistorik(folder_data_session, df):
     Mindre end 12 -> False
     """
     df["antal_gyldige_maaneder_ansfh03"] = ""
-    df["more_than_12_months_ansfh03"] = ""  # Initialize the column in df. TODO: test with empty string init.
+    df["more_than_12_months_ansfh03"] = ""
+    df["timeloen_pension_rettidig_dato"] = ""
 
     for cpr in df["cprnr"]:
         # parse and read
@@ -855,41 +862,155 @@ def process_all_anshistorik(folder_data_session, df):
                 year_month = year_month.where(day > 15, year_month + 1)  # noqa: PLR2004
                 return year_month
 
-            # Example usage:
             anshistorik["year_month"] = create_year_month(anshistorik["date"])
             anshistorik_grouped = anshistorik.groupby("year_month")["antal"].sum().reset_index()
 
             # Filter rows where antal > 34,67
-            monthly_hour_threshhold = 34.67
+            monthly_hour_threshhold = 34.67  # 34.67
+
             filtered_anshistorik_grouped = anshistorik_grouped[anshistorik_grouped["antal"] > monthly_hour_threshhold]
+
+            # if filtered_anshistorik_grouped has more than 12 rows, find the 13th year_month in the filtered DataFrame,
+            # convert to string and assign in to a variable
+            twelve = 12
+            if len(filtered_anshistorik_grouped) > twelve:
+                timeloen_pension_rettidig_dato = filtered_anshistorik_grouped.iloc[12]["year_month"].strftime("%Y-%m")
+            else:
+                timeloen_pension_rettidig_dato = ""
 
             antal_gyldige_maaneder_ansfh03 = len(filtered_anshistorik_grouped)
             # boolean value
-            twelve = 12
             more_than_12_months_ansfh03 = len(filtered_anshistorik_grouped) > twelve
 
             df.loc[df["cprnr"] == cpr, "more_than_12_months_ansfh03"] = more_than_12_months_ansfh03
             df.loc[df["cprnr"] == cpr, "antal_gyldige_maaneder_ansfh03"] = antal_gyldige_maaneder_ansfh03
+            df.loc[df["cprnr"] == cpr, "timeloen_pension_rettidig_dato"] = timeloen_pension_rettidig_dato
         else:
             df.loc[df["cprnr"] == cpr, "more_than_12_months_ansfh03"] = ""
             df.loc[df["cprnr"] == cpr, "antal_gyldige_maaneder_ansfh03"] = ""
+            df.loc[df["cprnr"] == cpr, "timeloen_pension_rettidig_dato"] = ""
 
     return df
 
 
 df = process_all_anshistorik(folder_data_session=folder_data_session, df=df)
 
-validate_dataframe(
-    dataframe=df, col_count=23, row_count=persistent_df_row_count, dataframe_name="df", manummer_column="manummer"
-)
 
+# ----------------------- Ansforhold output year_month ----------------------- #
+"""
+Bemærk: Er der udbetalt timeløn på 34,66 timer, eller over,
+i samme periode som månedsløn, tælles lønperioden kun én gang.
+"""
+
+
+# Denne funktion findes kun fordi vi skal sammenligne ansforhold og anshistorik.
+def stack_year_month_dfs_rem_dup(folder_data_session, bestillingsnavn, df):
+    df["ansfh01_og_03_gyldige_maaneder_uden_dub"] = ""
+
+    manummer_list = (df["manummer"]).tolist()
+    for manummer in manummer_list:
+        # --------------------- Convert ansforhold to year_month --------------------- #
+        df_ansforhold = read_single_ansforhold(manummer, folder_data_session, bestillingsnavn)
+
+        # Filter and sort the DataFrame
+        df_filtered = filter_df_ansforhold(df_ansforhold)
+        df_over_12_gyldige_ansfh01 = over_12_gyldige_ansfh01(df_filtered)[1]
+
+        if not df_over_12_gyldige_ansfh01.empty:
+            # select columns 'startdato_year_month', 'stopdato_year_month' from df_ansforhold
+            df_over_12_gyldige_ansfh01 = df_over_12_gyldige_ansfh01[["startdato_year_month", "stopdato_year_month"]]
+
+            # Initialize a list to hold the transformed data
+            rows = []
+
+            # Iterate over each row in the DataFrame
+            for _, row in df_over_12_gyldige_ansfh01.iterrows():
+                # Create a range of periods from start to stop date
+                period_range = pd.period_range(row["startdato_year_month"], row["stopdato_year_month"], freq="M")
+
+                # Append each period and the corresponding 'manr' to the list
+                for period in period_range:
+                    rows.append({"year_month": period})
+
+            # Convert the list of dictionaries into a DataFrame
+            df_over_12_gyldige_ansfh01 = pd.DataFrame(rows)
+
+            # add an empty column antal to df_over_12_gyldige_ansfh01
+            # to be able to stack it with filtered_anshistorik_grouped
+            df_over_12_gyldige_ansfh01["antal"] = ""
+
+        # Get filtered_anshistorik_grouped
+        cpr = df.loc[df["manummer"] == manummer, "cprnr"].values[0]
+        mhtml_path = Path(folder_data_session / f"anshistorik_{cpr}.mhtml")
+        anshistorik = parse_ri_html_report_to_dataframe(mhtml_path)
+
+        if anshistorik is not None and not anshistorik.empty:
+            # remove rows where
+
+            # Create a year_month column, depending on the date column.
+            # If date is between day 16 and 31, then set year_month to the next month.
+            # If date is between day 1 and 15, then set year_month to the current month.
+            def create_year_month(date_column):
+                year_month = pd.to_datetime(date_column).dt.to_period("M")
+                day = pd.to_datetime(date_column).dt.day
+                year_month = year_month.where(day > 15, year_month + 1)  # noqa: PLR2004
+                return year_month
+
+            anshistorik["year_month"] = create_year_month(anshistorik["date"])
+            anshistorik_grouped = anshistorik.groupby("year_month")["antal"].sum().reset_index()
+
+            # Filter rows where antal > 34,67
+            monthly_hour_threshhold = 34.67  # 34.67
+
+            filtered_anshistorik_grouped = anshistorik_grouped[anshistorik_grouped["antal"] > monthly_hour_threshhold]
+
+            # bind / stack filtered_anshistorik_grouped to df_over_12_gyldige_ansfh01
+            # convert antal to float in filtered_anshistorik_grouped
+            filtered_anshistorik_grouped.loc[:, "antal"] = filtered_anshistorik_grouped["antal"].astype(float)
+
+        stacked = pd.concat([df_over_12_gyldige_ansfh01, filtered_anshistorik_grouped], axis=0)
+        # remove duplicates friom stacked
+        stacked = stacked.drop_duplicates(subset=["year_month"], keep="first")
+        stacked_row_count = stacked.shape[0]
+
+        df.loc[df["manummer"] == manummer, "ansfh01_og_03_gyldige_maaneder_uden_dub"] = stacked_row_count
+
+    return df
+
+
+df = stack_year_month_dfs_rem_dup(folder_data_session, bestillingsnavn, df)
+
+
+# Save the DataFrame to a CSV file
+df.to_csv(path_or_buf=Path(folder_data_session / "df.csv"), index=False, encoding="windows-1252", sep=";")
+
+
+# Close the SAP session
+
+
+# ------------------------------------ End ----------------------------------- #
 
 # Kontroller: Er der udbetalt timeløn på 34,66 timer, eller over, i samme periode(r)
 # som månedsløn, tælles lønperioden kun én gang!
 
 # Beregn dato for pension: den "12 måned + 1"
 
+# Hvad hvis noget gå galt hvordan starter vi fra det punk vi slap?
+
 # måske problem: "er medarbejder oprettet med pension på månedsløn" udføres efter alle filtrene er kørt i gennem.
 
-# Lektie til næste robot. Lav en processbeskrivelse der både kører rækkevis (personvis) og kolonnevis (funktionsvis)
+# Lektie til næste robot. Overvej grundigt om kørsel rækkevis eller kolonnevis.
+# Processbeskrivelse der både kører rækkevis (personvis) og kolonnevis (funktionsvis).
+# Hvad har det af betydning i forhold til at at gentage processen fra et bestemt punkt.
 # Lav også en beskrivelse af datastrukturens input og output. Fx tabel.shape
+# Alt i alt: beskriv krav og hvor data ligger. Ikke hvodan det skal kodes.
+
+end_time = time.time()
+# round float to whole number
+duration = round(end_time - start_time)
+
+logger.info(
+    f"the session took {duration} seconds "
+    f"with {persistent_df_row_count} medarbejdere "
+    f"in downloadmode {downloadmode} on {server_name}"
+)
